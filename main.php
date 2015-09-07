e604b983f7dabe0fc0db75c3cfa8ec5ddf97689d
Douglas Muth <dmuth@dmuth.org>
2015-09-07 14:05:58 -0400

    Removed old code

--- main.php
+++ main.php
@@ -15,7 +15,7 @@ $num_games = 5;
 $table = new Craps\Table($logger, $num_games);
 //$table->debugSet("rolls", array(7,7,7));
 //$table->debugSet("rolls", array(7,7,6));
-$table->debugSet("rolls", array(2,7,8,7,8,8,7));
+//$table->debugSet("rolls", array(2,7,8,7,8,8,7));
 
 $strategy = array(
 	"bet" => 10,
@@ -23,7 +23,7 @@ $strategy = array(
 	);
 
 $players = array();
-//$players[] = new Craps\Player($logger, 100, $strategy);
+$players[] = new Craps\Player($logger, 100, $strategy);
 
 foreach ($players as $key => $value) {
 	$table->addPlayer($value);
@@ -35,28 +35,6 @@ while (true) {
 		break;
 	}
 }
-
-/*
-$game_id = "";
-$game_id_old = "";
-while (true) {
-
-	$table->roll();
-	$game_id = $table->getGameId();
-	if (!$game_id_old) {
-		$logger->info("New (first) game: $game_id");
-		$game_id_old = $game_id;
-	}
-
-	if ($game_id != $game_id_old) {
-		$logger->info("New game: $game_id");
-		$game_id_old = $game_id;
-		$i++;
-	}
-
-} // while()...
-*/
-
 
 $logger->info("Table stats: " . json_encode($table->getStats()));
 
