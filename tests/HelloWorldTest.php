232861ed219f46271b67e2e77abcc89f908c4a39
Douglas Muth <dmuth@dmuth.org>
2015-09-15 20:12:12 -0400

    Added setup() and teardown().

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -2,6 +2,13 @@
 
 class HelloWorldTest extends PHPUnit_Framework_TestCase {
 
+	protected function setup() {
+		$this->key = "value";
+	}
+
+	protected function teardown() {
+		$this->key = "";
+	}
 
     public function testHello() {
 
@@ -11,6 +18,8 @@ class HelloWorldTest extends PHPUnit_Framework_TestCase {
 		$this->assertFalse(false);
 		$this->assertGreaterThan(1, 2);
 		$this->assertLessThan(2, 1);
+
+		$this->assertEquals($this->key, "value");
 
     } // End of testHello()
 
